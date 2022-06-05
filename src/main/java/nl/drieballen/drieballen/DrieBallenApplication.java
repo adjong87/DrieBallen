519839260dc7da341559c28b20e145041a3ba3b0
adjong87 <adjong87@gmail.com>
2022-06-05 04:24:06 +0200

    Het echte werk gaat beginnen

---
+++ src/main/java/nl/drieballen/drieballen/DrieBallenApplication.java
@@ -0,0 +1,13 @@
+package nl.drieballen.drieballen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrieBallenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrieBallenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/drieballen/drieballen/DrieBallenApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.drieballen.drieballen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrieBallenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
