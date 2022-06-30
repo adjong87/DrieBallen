9bd56e6e08dedc3148f931ee7c1a1b31ebdfa9dd
adjong87 <adjong87@gmail.com>
2022-06-30 16:35:41 +0200

    tussentijdse opslag

--- src/main/java/nl/drieballen/drieballen/controllers/ProfileController.java
+++ src/main/java/nl/drieballen/drieballen/controllers/ProfileController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping(value = "/members")
+@RequestMapping(value = "/profiles")
 public class ProfileController {
 
     private final ProfileService profileService;
